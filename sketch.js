9e003399471df356090b8eef8067959c4f3f44a9
codestudent001 <codestudent001@gmail.com>
2020-11-11 16:03:38 +0530

    high

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,9 +35,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(100,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    slingshot = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    slingshot.display();
+    log6.display();
 }
